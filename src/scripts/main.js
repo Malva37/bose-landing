06de388978d7b79e9619b913daf638afb32a0eae
Iryna Pysanko <malva37@gmail.com>
2023-02-10 12:43:41 +0200

    clone bose landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const btnContactUs = document.querySelector('.contact-us__button');
+
+btnContactUs.addEventListener('click', checkboxClick, false);
+
+function checkboxClick(event) {
+  event.preventDefault();
+}
